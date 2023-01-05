90f7c76418ab18092cfd15de855360d4f83359c0
navabgaznabi <nav.gaznabi@gmail.com>
2023-01-06 00:17:38 +0530

    Converted to functional Component

--- src/components/Monster.comp.jsx
+++ src/components/Monster.comp.jsx
@@ -1,45 +1,58 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import { useEffect, useState } from "react";
 import SearchInput from "./input-field/SearchInput.comp";
 import MonsterList from "./monsterList/MonsterList.comp";
 
-class Monster extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      stateField: [],
-    };
-  }
-
-  componentDidMount() {
+const Monster = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [stateField, setStateField] = useState("");
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((res) => {
-        this.setState(() => {
-          return { monsters: res };
-        });
+        setMonsters(res);
       })
       .catch((err) => console.log("Failed to Fetch Monster"));
-  }
-
-  inputSearchHandler = (event) => {
-    const stateField = event.target.value.toLowerCase();
-    this.setState({ stateField });
+  }, []);
+  const inputSearchHandler = (event) => {
+    const stateFieldString = event.target.value.toLowerCase();
+    setStateField(stateFieldString);
   };
+  const filterMonster = monsters.filter((monster) => {
+    return monster.name.toLowerCase().includes(stateField);
+  });
 
-  render() {
-    const { monsters, stateField } = this.state;
-    const { inputSearchHandler } = this;
-    const filterMonster = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(stateField);
-    });
-    return (
-      <>
-        <h1 className="title">Monster Paradise</h1>
-        <SearchInput inputSearchHandler={inputSearchHandler} />
-        <MonsterList filterMonster={filterMonster} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1 className="title">Monster Paradise</h1>
+      <SearchInput inputSearchHandler={inputSearchHandler} />
+      <MonsterList filterMonster={filterMonster} />
+    </>
+  );
+};
+
+// class Monster extends Component {
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((res) => {
+//         this.setState(() => {
+//           return { monsters: res };
+//         });
+//       })
+//       .catch((err) => console.log("Failed to Fetch Monster"));
+//   }
+
+//   render() {
+//     const { monsters, stateField } = this.state;
+//     const { inputSearchHandler } = this;
+//     const filterMonster = monsters.filter((monster) => {
+//       return monster.name.toLowerCase().includes(stateField);
+//     });
+//     return (
+//
+//     );
+//   }
+// }
 export default Monster;
